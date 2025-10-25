e2f768e08e03c49b384050ce3fcaf0ff135b7525
Martin Faverio <145507051+MartinFaverio@users.noreply.github.com>
2025-10-24 23:38:07 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ load_dotenv(dotenv_path='data.env')
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@localhost/{os.getenv('DB_NAME')}"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
